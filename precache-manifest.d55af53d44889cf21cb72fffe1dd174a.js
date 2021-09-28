79a94d92e3a6e7a32a5847716a5e28b71dd3b6c0
Stephanie Blanchard <sblanchard2432@gmail.com>
2021-09-28 17:55:22 -0400

    Updates

---
+++ precache-manifest.d55af53d44889cf21cb72fffe1dd174a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199c6411c247efebb1d4583d10661597",
+    "url": "/book-search/index.html"
+  },
+  {
+    "revision": "d76d1b3c1eebf7511869",
+    "url": "/book-search/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "2174551236fe0dd9b017",
+    "url": "/book-search/static/css/main.3660f5ee.chunk.css"
+  },
+  {
+    "revision": "d76d1b3c1eebf7511869",
+    "url": "/book-search/static/js/2.2ff09b52.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/book-search/static/js/2.2ff09b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2174551236fe0dd9b017",
+    "url": "/book-search/static/js/main.271875a7.chunk.js"
+  },
+  {
+    "revision": "fbcc8bb33cc4fd62835c",
+    "url": "/book-search/static/js/runtime-main.4e877960.js"
+  }
+]);
